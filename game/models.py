15d7d3086a639445145b4577de414da3f8fccbb3
Matthew Wyatt <matthew.a.wyatt@gmail.com>
2011-10-20 15:08:41 -0700

    Moved GameState fields into Game

--- game/models.py
+++ game/models.py
@@ -7,16 +7,13 @@ class Unit(models.Model):
     ypos = models.IntegerField()
     unit_type = models.CharField(max_length=30) # or do we want this in another table?
 
-class GameState(models.Model):
-    active_player = models.ForeignKey(User)
-    turn = models.IntegerField()
-    units = models.ManyToManyField(Unit)
-
 class Game(models.Model):
     name = models.CharField(max_length=50, default='MSG')
     mapname = models.CharField(max_length=20)
     players = models.ManyToManyField(User)
-    state = models.ForeignKey(GameState)
+    active_player = models.ForeignKey(User)
+    turn = models.IntegerField(default=1)
+    units = models.ManyToManyField(Unit)
     joined = models.BooleanField()
     completed = models.BooleanField()
 
