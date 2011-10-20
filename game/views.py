798ff7771b0cd846a4271b9879135a9dee230c25
Matthew Wyatt <matthew.a.wyatt@gmail.com>
2011-10-20 14:02:48 -0700

    moved the start game function here; still need to figure out protocol for how games will get started

--- game/views.py
+++ game/views.py
@@ -1,1 +1,22 @@
-# Create your views here.
+from models import Game
+#TODO:
+#new plan: registration is distinct from game creation. All game creation is done on a single page, and only by registered users
+def startgame(request):
+    if request.method == 'POST':
+        #TODO: DANGER NOT SANITIZED!
+        address = self.request.get("address")
+        logger.info('Trying to start game with email address: ' + address)
+        if request.user:
+            gs = game.init_game_state(request.user)
+            game = Game(mapname='basic',joined=False,completed=False, state=gs)
+            game.save()
+            game.users.add(request.user)
+        ## send_mail(subject="You're invited to play MSG!",
+        ##           message="You've been invited ")
+        else:
+            pass #need to redirect? blow up?
+        # TODO: make idempotent
+        return HttpResponseRedirect('/')
+    else:
+        pass #TODO?
+
