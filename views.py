ac3778aa2faa5e3f426511b7439b73dffdfd972f
Matthew Wyatt <matthew.a.wyatt@gmail.com>
2011-10-20 14:04:29 -0700

    removed startgame; changed way game list is passed to template

--- views.py
+++ views.py
@@ -7,37 +7,18 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.template import RequestContext
 import logging
-from game.models import Game
-from game.game import init_game_state
+
+#list of game-specific modules to import
+import game
 
 logger = logging.getLogger(__name__)
 
 @login_required
 def main_view(request):
-    games = Game.objects.filter(players__id=request.user.id)
+    games = []
+    games.extend(game.models.listgames(request.user))
     return render_to_response('index.html', {'username':request.user.username, 'games':games},
                               context_instance=RequestContext(request))
-
-#TODO:
-#new plan: registration is distinct from game creation. All game creation is done on a single page, and only by registered users
-def startgame(request):
-    if request.method == 'POST':
-        #TODO: DANGER NOT SANITIZED!
-        address = self.request.get("address")
-        logger.info('Trying to start game with email address: ' + address)
-        if request.user:
-            gs = game.init_game_state(request.user)
-            game = Game(mapname='basic',joined=False,completed=False, state=gs)
-            game.save()
-            game.users.add(request.user)
-        ## send_mail(subject="You're invited to play MSG!",
-        ##           message="You've been invited ")
-        else:
-            pass #need to redirect? blow up?
-        # TODO: make idempotent
-        return HttpResponseRedirect('/')
-    else:
-        pass #TODO?
 
 def register(request):
     if request.method == 'POST':
