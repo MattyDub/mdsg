439bcb6a9cc921c8c78277537f0437040a8f88dd
Matthew Wyatt <matthew.a.wyatt@gmail.com>
2011-10-21 09:50:01 -0700

    changed module name

--- settings.py
+++ settings.py
@@ -120,7 +120,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'registration',
-    'djmsg.game',
+    'djmsg.msg',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
