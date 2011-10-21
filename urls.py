83a2dab0c41ec4deac7291d656745beb4711bc00
Matthew Wyatt <matthew.a.wyatt@gmail.com>
2011-10-21 12:00:55 -0700

    removed silly comments; added newgame handler'; now including msg urls

--- urls.py
+++ urls.py
@@ -1,20 +1,19 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 from django.contrib.auth.views import login, logout
-from djmsg.views import main_view, register
+from djmsg.views import main_view, register, newgame
 from django.conf import settings
+import msg
 
 admin.autodiscover()
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
 
 urlpatterns = patterns('',
                        (r'^accounts/login/$', login, {'template_name':'login.html'}),
                        (r'^accounts/logout/$', logout, {'template_name':'login.html'}),
+                       (r'^register/$', register),
                        (r'^$', main_view),
-                       (r'^register/', register),
+                       (r'^newgame/$', newgame),
+                       (r'^msg/', include('msg.urls')),
                        url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
                        url(r'^admin/', include(admin.site.urls)),
 #                       (r'^static/(?P<path>.*)$', 'django.views.static.serve',
